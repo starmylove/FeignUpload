a8b689c07ede514b3ca917381a4d99ae6bf48a3e
passion-logan <wangdadamailbox@163.com>
2019-11-26 11:14:05 +0800

    添加serverr

---
+++ eureka-feign-upload-server/src/test/java/com/cody/EurekaFeignUploadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaFeignUploadServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
