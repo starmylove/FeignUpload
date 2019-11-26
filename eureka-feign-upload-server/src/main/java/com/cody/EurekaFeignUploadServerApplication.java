504eaaca86dcb55b226f86628b8a3e328c7d4dad
passion-logan <wangdadamailbox@163.com>
2019-11-26 11:39:17 +0800

    修改server

--- eureka-feign-upload-server/src/main/java/com/cody/EurekaFeignUploadServerApplication.java
+++ eureka-feign-upload-server/src/main/java/com/cody/EurekaFeignUploadServerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -17,7 +19,9 @@ public class EurekaFeignUploadServerApplication {
     public class UploadController {
 
         @PostMapping(value = "uploadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-        public String handleFileUpload()
+        public String handleFileUpload(@RequestPart(value = "file") MultipartFile file) {
+            return file.getName();
+        }
     }
 
     public static void main(String[] args) {
