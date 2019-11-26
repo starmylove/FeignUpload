26317b09d869ba3bf7e0fd39580d351883a8ad08
passion-logan <wangdadamailbox@163.com>
2019-11-26 11:24:03 +0800

    添加上传配置

--- eureka-feign-upload-server/src/main/java/com/cody/EurekaFeignUploadServerApplication.java
+++ eureka-feign-upload-server/src/main/java/com/cody/EurekaFeignUploadServerApplication.java
@@ -2,9 +2,23 @@ package com.cody;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaFeignUploadServerApplication {
+
+    @RestController
+    public class UploadController {
+
+        @PostMapping(value = "uploadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+        public String handleFileUpload()
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaFeignUploadServerApplication.class, args);
