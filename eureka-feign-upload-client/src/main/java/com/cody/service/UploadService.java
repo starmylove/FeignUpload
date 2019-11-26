f9a0ae6aaeeb8563b7f991db7a7f20d70719e2d2
passion-logan <wangdadamailbox@163.com>
2019-11-26 12:11:04 +0800

    添加service

---
+++ eureka-feign-upload-client/src/main/java/com/cody/service/UploadService.java
@@ -0,0 +1,34 @@
+package com.cody.service;
+
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年11月26日 0026 11:59
+ */
+@FeignClient(value = "eureka-feign-upload-server", configuration = UploadService.MultipartSupportConfig.class)
+public interface UploadService {
+
+    @PostMapping(value = "uploagFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    String handleFileUpload(@RequestPart(value = "file") MultipartFile file);
+
+    @Configuration
+    class MultipartSupportConfig {
+        @Bean
+        public Encoder feignFormEncoder() {
+            return new SpringFormEncoder();
+        }
+    }
+}
